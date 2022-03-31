f73aa58d1091bb80aacfbac4d23b54f67a4d18aa
alweso <alweso@gmail.com>
2022-03-31 17:06:39 +0200

    removed unnecessary CSS

--- src/components/Hexagon/Hexagon.tsx
+++ src/components/Hexagon/Hexagon.tsx
@@ -5,7 +5,7 @@ const Hexagon: React.FC<HexagonType> = (props) => {
 	const { color } = props
 	return (
 		<div className="hexagon" style={{"height": "100px",display: "inline-block", position: "relative"}}>
-		<svg viewBox="0 0 200 160" style={{display: "inline-block", height: "100px", position: "relative", width: "auto"}}>
+		<svg viewBox="0 0 200 160" style={{height: "100px"}}>
 				<polygon fill={color} points="138,166.33 52,166.33 4,83.2 52,0 138,0 186,83.2" />
 		</svg>
 
--- src/components/Triangle/Triangle.tsx
+++ src/components/Triangle/Triangle.tsx
@@ -5,7 +5,7 @@ const Triangle: React.FC<TriangleType> = (props) => {
 	const { color } = props
 	return (
 		<div className="triangle" style={{"height": "100px",display: "inline-block", position: "relative"}}>
-			<svg viewBox="0 0 240 200" width="240" height="200" style={{position: "relative",width:"100%",height: "100%", display: "inline-block"}}>
+			<svg viewBox="0 0 240 200" width="240" height="200" style={{position: "relative",width:"100%",height: "100%"}}>
 				<polygon points="0,200 120,0 240,200" fill={color} />
 			</svg>
 		</div>
